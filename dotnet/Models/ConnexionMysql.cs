789fe2c01790409fd9af0e8a0e2a3b8a0e6b0f2f
Miantsa2 <ranaivosonmiantsa@gmail.com>
2024-12-20 10:50:29 +0300

    commit

--- dotnet/Models/ConnexionMysql.cs
+++ dotnet/Models/ConnexionMysql.cs
@@ -11,8 +11,8 @@ namespace connexion.Models
             try
             {
                 // Définir la chaîne de connexion MySQL
-                string connectionString = "Server=localhost;Port=3305;Database=cloud;User=root;Password=root;";
-;
+                string connectionString = "Server=localhost;Port=3306;Database=cloud;User=root;Password=root;";
+
 
                 // Créer la connexion
                 connexion = new MySqlConnection(connectionString);
